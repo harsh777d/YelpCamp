32040f6170b9072a1fb43b1a701c83daf4a1e30b
harsh <harshbeprime@gmail.com>
2020-08-05 09:35:56 +0530

    v1

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get('/', (req,res) =>{
+    res.render("landing");
+});
+
+app.get('/new', (req,res) =>{
+    res.render("new");
+});
+
+app.get("/campgrounds",(req,res)=>{
+    var campgrounds = [
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1483381719261-6620dfa2d28a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1483381719261-6620dfa2d28a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1483381719261-6620dfa2d28a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name :"Salmon Creek", image:"https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    ]
+
+    res.render("campgrounds",{campgrounds:campgrounds});
+})
+
+
+app.listen(3000,'localhost', ()=>{
+    console.log("YelpCamp server has been started");
+});
