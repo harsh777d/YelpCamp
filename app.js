c71936fc34a2275b43aba2f10075509194d13e5d
harsh <harshbeprime@gmail.com>
2020-08-23 23:00:38 +0530

    added environmental variables

--- app.js
+++ app.js
@@ -39,12 +39,13 @@ app.use(function(req,res,next){
     next();
 });
 
-mongoose.connect("mongodb://localhost/yelp_camp",{
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser : true,
     useUnifiedTopology : true
 })
 .then(()=> console.log('connected to db!'))
 .catch(error => console.log(error.message));
+
 
 //seedDB();
 
